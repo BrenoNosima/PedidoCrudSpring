37ceda1d6ab78e270befa001f3b7a7ca45b74daf
Nosima <breno.nosima@gmail.com>
2026-03-19 21:27:37 -0300

    first commit

---
+++ src/main/java/com/bn/pedidoscs/PedidosCsApplication.java
@@ -0,0 +1,13 @@
+package com.bn.pedidoscs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosCsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PedidosCsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bn/pedidoscs/PedidosCsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bn.pedidoscs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosCsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
